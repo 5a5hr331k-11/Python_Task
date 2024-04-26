504d26a31e9ee1168a2f51da703b47352e33253f
5a5hr331k-11 <167100691+5a5hr331k-11@users.noreply.github.com>
2024-04-26 18:34:18 +0530

    Update Looping task.py

--- Looping task.py
+++ Looping task.py
@@ -3,12 +3,12 @@
 for number in range(1,16,1):
     print(number)
 
-#Task 2
+Task 2
 
 for odd in range(7,30,2):
     print(odd, "odd no.")
 
-#Task 3
+###Task 3
 
 for even in range(20,41,2):
     print(even, "even no.")
@@ -16,29 +16,30 @@ for even in range(20,41,2):
 #Task 4
 
 for d in range(1,16,1):
-    if d % 3 == 0:
-        print(d,"divisible by 3")
+    if d % 5 == 0 and d % 3 == 0:
+        print(d,"divisible by both 3 and 5")
+       
 
     elif d % 5 == 0:
         print(d,"divisible by 5")
 
-    elif d % 5 == 0 and d % 3 == 0:
-        print(d,"divisible by both 3 and 5")
+    elif d % 3 == 0:
+         print(d,"divisible by 3")
 
     else:
         print(d,"nil")
 
 #Task 5
 
-l=["cricket" ,"basketball" ,"tennis" ,"badminton" ,"volleyball" ]
-for sports in l:
-    print(l,"sport")
+Sports= ["cricket","basketball","tennis","badminton","volleyball"]
+
+for S in Sports:
+    print(S,"sport")
 
 #Task 7
 
 sum=0
 for x in range(1,101,1):
     sum=sum+x
-    print(x)
 
-        
+print(sum:"is the sum of 1 - 100")
