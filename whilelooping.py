7390a6d7fc077882c97b964d6d2071c24ec6846c
5a5hr331k-11 <167100691+5a5hr331k-11@users.noreply.github.com>
2024-04-29 18:11:51 +0530

    Add files via upload

---
+++ whilelooping.py
@@ -0,0 +1,30 @@
+#Task 1
+n=0
+while n<15:
+    n+=1
+    print(n)
+
+#Task 2
+i=5
+while i<29:
+    i+=2
+    print(i)
+
+#Task 3
+eve=18
+while eve<40:
+    eve+=2
+    print(eve)
+
+#Task 4
+num=0
+while num<15:
+    num+=1
+if num%3==0 and num%5==0:
+    print(num,"divisible by 3 and 5")
+    elif num%3==0:
+        print(num,"divisible by 3")
+        elif num%5==0:
+            print(num,"divisible by 5")
+            else:
+                print("null")
