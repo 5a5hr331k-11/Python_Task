8b134a7e9d9ab8204ef33f1c26dd7372e6282f9e
5a5hr331k-11 <167100691+5a5hr331k-11@users.noreply.github.com>
2024-05-02 18:49:03 +0530

    Update whilelooping.py

--- whilelooping.py
+++ whilelooping.py
@@ -2,29 +2,30 @@
 n=0
 while n<15:
     n+=1
-    print(n)
+    print(n,"number")
 
 #Task 2
 i=5
 while i<29:
     i+=2
-    print(i)
+    print(i,"odd")
 
 #Task 3
 eve=18
 while eve<40:
     eve+=2
-    print(eve)
+    print(eve,"even")
 
 #Task 4
 num=0
 while num<15:
     num+=1
-if num%3==0 and num%5==0:
-    print(num,"divisible by 3 and 5")
+    if num%3==0 and num%5==0:
+        print(num,"divisible by 3 and 5")
     elif num%3==0:
         print(num,"divisible by 3")
-        elif num%5==0:
-            print(num,"divisible by 5")
-            else:
-                print("null")
+    elif num%5==0:
+        print(num,"divisible by 5")
+    else:
+        print(num)
+
