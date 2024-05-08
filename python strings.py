43550261ec85e59ff09d723448c230cb269ee1f0
5a5hr331k-11 <167100691+5a5hr331k-11@users.noreply.github.com>
2024-05-08 19:36:22 +0530

    Add files via upload

---
+++ python strings.py
@@ -0,0 +1,76 @@
+#Slice:
+
+name = "ocean academy"
+print(name[1:8])
+
+#Uppercase:
+
+print(name.upper())
+
+#Lowercase:
+
+print(name.lower())
+
+#Replace:
+
+print(name.replace("c","l"))
+
+#Length of string:
+
+print(len(name))
+
+#Strip:
+
+x = "  ocean academy  "
+print(x.strip())
+
+#l strip:
+
+print(x.lstrip())
+
+#r strip:
+
+print(x.rstrip())
+
+#Capitalize:
+
+print(name.capitalize())
+
+#Split:
+
+print(name.split( ))
+
+#Casefold:
+
+print(name.casefold())
+
+#Count:
+
+Count = "the number of offers I recieve everyday are endless"
+print(Count.count("of"))
+
+#endswith:
+
+print(name.endswith("y"))
+
+#startswith:
+
+print(name.startswith("o"))
+
+#Find:
+
+print(name.find("aaaa"))
+
+#Title:
+
+print(name.title())
+
+#Swapcase:
+
+print(name.swapcase())
+
+#Splitlines:
+
+String = "the number of offers\n I recieve everyday\n are endless"
+print(String.splitlines())
+
