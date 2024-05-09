ef342bf974fa873fdd436c36ce1a32a77c883c53
5a5hr331k-11 <167100691+5a5hr331k-11@users.noreply.github.com>
2024-05-09 19:53:55 +0530

    Add files via upload

---
+++ ListMethods.py
@@ -0,0 +1,63 @@
+#Append:
+
+myList = ["hydrogen","helium","lithium"]
+myList.append("berylium")
+print(myList)
+
+#Extend:
+
+myList = ["hydrogen","helium","lithium"]
+Fruits = ["apple","orange","lemon"]
+myList.extend(Fruits)
+print(myList)
+
+#Clear:
+
+myList = ["hydrogen","helium","lithium"]
+myList.clear()
+print(myList)
+
+#Pop:
+
+myList = ["hydrogen","helium","lithium"]
+myList.pop(1)
+print(myList)
+
+#Insert:
+
+myList = ["hydrogen","helium","lithium"]
+myList.insert(1,"berylium")
+print(myList)
+
+#Copy:
+
+myList = ["hydrogen","helium","lithium"]
+myList.copy()
+print(myList)
+
+#Count:
+
+elements = ["hydrogen","helium","helium","lithium"]
+print(elements.count("helium"))
+
+#Sort:
+
+myList = ["hydrogen","helium","lithium"]
+myList.sort()
+print(myList)
+
+#Reverse:
+
+myList = ["hydrogen","helium","lithium"]
+myList.reverse()
+print(myList)
+
+#Index:
+
+myList = ["hydrogen","helium","lithium"]
+print(myList.index("lithium"))
+
+
+
+
+
