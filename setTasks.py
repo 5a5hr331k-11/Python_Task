ee93cd3f1b050029eadbed93286e689586b95668
5a5hr331k-11 <167100691+5a5hr331k-11@users.noreply.github.com>
2024-05-21 19:37:51 +0530

    Add files via upload

---
+++ setTasks.py
@@ -0,0 +1,35 @@
+# Size of a set:
+My_set = {"Football","Cricket","Basketball","Chess"}
+print(len(My_set))
+
+# Maximum and Minimum of a set:
+numSet = {12,43,7,45,17}
+convSet = list(numSet)
+convSet.sort()
+print(convSet[0],"minimum",convSet[-1],"maximum")
+
+#Task 1:
+consSet = set(("Football","Cricket","Basketball","Chess"))
+print(consSet)
+
+#Task 2:
+for x in My_set:
+    print(x)
+
+#Task 3:
+My_set.add("Volleyball")
+print(My_set)
+
+#Task 4:
+My_set.remove("Chess")
+print(My_set)
+
+#Task 5:
+second_set = {7,45,89,60,34}
+numSet.intersection(second_set)
+print(numSet)
+
+
+
+
+
