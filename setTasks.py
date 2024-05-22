cf7839aed7f6b57b4923668cb81e381dc9846021
5a5hr331k-11 <167100691+5a5hr331k-11@users.noreply.github.com>
2024-05-22 16:09:02 +0530

    Update setTasks.py

--- setTasks.py
+++ setTasks.py
@@ -25,10 +25,35 @@ My_set.remove("Chess")
 print(My_set)
 
 #Task 5:
+numSet = set(convSet)
 second_set = {7,45,89,60,34}
-numSet.intersection(second_set)
-print(numSet)
+for item in numSet:
+    if item in second_set:
+        numSet.remove(item)
+        print(numSet)
 
+#Task 6:
+DemoSet = {"Youtube","Twitter","Instagram","Mail"}
+Demo2Set = {"Fax","Mail","Messages","Telephone"}
+DemoSet.intersection(Demo2Set)
+print(DemoSet)
+
+#Task 7:
+DemoSet.difference(Demo2Set)
+print(DemoSet)
+
+#Task 8:
+subSet = {"Football","Cricket"}
+print(subSet.issubset(My_set))
+
+#Task 9:
+print("Cricket" in consSet)
+
+#Task 10:
+for values in My_set:
+    if values not in DemoSet:
+        print("there are no common elements")
+        
 
 
 
