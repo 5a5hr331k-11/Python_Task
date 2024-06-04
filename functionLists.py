b99e8adeb3fdb5c1efbb4a003b6843177580cca5
5a5hr331k-11 <167100691+5a5hr331k-11@users.noreply.github.com>
2024-06-04 19:32:25 +0530

    Add files via upload

---
+++ functionLists.py
@@ -0,0 +1,27 @@
+# Task - 1:
+myList = [12,45,87,1,5]
+def addNum(Total):
+    sum = 0
+    for n in Total:
+        sum += n
+    return sum
+print(addNum(myList))
+
+# Task - 2:
+def MaxElement(Maximum):
+    Largest = Maximum[0]
+    for Max in Maximum[1:]:
+        if Max > Largest:
+            Largest = Max
+    return Largest
+print(MaxElement(myList))
+
+# Task - 3
+def MinElement(Minimum):
+    Smallest = Minimum[0]
+    for Min in Minimum[1:]:
+        if Min < Smallest:
+            Smallest = Min
+    return Smallest
+print(MinElement(myList))
+        
